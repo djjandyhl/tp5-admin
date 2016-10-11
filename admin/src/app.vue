f6308a1a6ef905e6be8e0239da6b107a18bbabc1
DaiJunJie <236669375@qq.com>
2016-10-11 10:22:28 +0800

    111

---
+++ admin/src/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      startHacking() {
+        this.$notify({
+          title: 'It Works',
+          message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
+          duration: 6000
+        });
+      }
+    },
+    data: function() {
+      return {
+        msg: 'my first test!'
+      };
+    }
+  };
+</script>
+<style>
+  body{padding: 0;margin: 0}
+</style>
---
+++ cooking.conf.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var cooking = require('cooking');
+
+cooking.set({
+  entry: {
+    app: ['babel-polyfill', './admin/src/main.js']
+  },
+  dist: './admin/dist',
+  template: './admin/index.tpl',
+
+  devServer: {
+    port: 8080,
+    publicPath: '/',
+    stats: 'info',
+    noInfo: true,
+    quiet: false,
+    lazy: false,
+    // HTML5 history API
+    historyApiFallback: true,
+    log: false,
+    // 热替换
+    hot: true
+  },
+
+  // production
+  clean: true,
+  hash: true,
+  sourceMap: true,
+  minimize: true,
+  chunk: true, // see https://cookingjs.github.io/zh-cn/configuration.html#chunk
+  postcss: [
+    // require('...')
+  ],
+  publicPath: './',
+  assetsPath: 'static',
+  urlLoaderLimit: 10000,
+  extractCSS: '[name].[contenthash:7].css',
+  alias: {
+    'src': path.join(__dirname, 'src')
+  },
+  extends: ['vue2', 'lint', 'saladcss']
+});
+
+module.exports = cooking.resolve();
